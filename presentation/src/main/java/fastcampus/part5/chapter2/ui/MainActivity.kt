277da09d9c99c256479e469630577767c078f0be
Latte <fsd.latte@gmail.com>
2023-03-05 23:34:30 +0900

    [Clip06] Main화면 하단에 Tab추가 및 Fragment 생성

--- presentation/src/main/java/fastcampus/part5/chapter2/ui/MainActivity.kt
+++ presentation/src/main/java/fastcampus/part5/chapter2/ui/MainActivity.kt
@@ -8,10 +8,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import fastcampus.part5.chapter2.ui.theme.MyApplicationTheme
 import fastcampus.part5.chapter2.viewmodel.TempViewModel
@@ -19,31 +16,17 @@ import fastcampus.part5.chapter2.viewmodel.TempViewModel
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel : TempViewModel by viewModels()
+//    private val viewModel : TempViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "TempValue=${viewModel.getTempModel().name}", Toast.LENGTH_SHORT).show()
 
         setContent {
             MyApplicationTheme {
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
--- presentation/src/main/java/fastcampus/part5/chapter2/ui/MainScreen.kt
+++ presentation/src/main/java/fastcampus/part5/chapter2/ui/MainScreen.kt
@@ -1,4 +1,103 @@
 package fastcampus.part5.chapter2.ui
 
-class MainScreen {
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import fastcampus.part5.chapter2.ui.theme.MyApplicationTheme
+import fastcampus.part5.di.R
+
+
+sealed class MainNavigationItem(var route: String, var name: String) {
+    object Main : MainNavigationItem("Main","Main")
+    object Category : MainNavigationItem("Category","Category")
+    object MyPage : MainNavigationItem("MyPage","MyPage")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        MainScreen()
+    }
+}
+
+@Composable
+fun MainScreen() {
+    val scaffoldState = rememberScaffoldState()
+    val navController = rememberNavController()
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        bottomBar = {
+            MainBottomNavigationBar(navController)
+        }
+    ) {
+        MainNavigationScreen(navController = navController)
+    }
+
+}
+
+@Composable
+fun MainBottomNavigationBar(navController: NavHostController) {
+    val bottomNavigationItems = listOf(
+        MainNavigationItem.Main,
+        MainNavigationItem.Category,
+        MainNavigationItem.MyPage,
+    )
+
+    BottomNavigation(
+        backgroundColor = Color(0xffff0000),
+        contentColor = Color(0xff00ff00)
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        bottomNavigationItems.forEach {  item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = R.drawable.ic_launcher_foreground), item.route)},
+                selected = currentRoute == item.route
+                , onClick = {
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let {
+                            popUpTo(it) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+
+                        restoreState = true
+                    }
+
+                })
+            }
+    }
+}
+
+@Composable
+fun MainNavigationScreen(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = MainNavigationItem.Main.route) {
+        composable(MainNavigationItem.Main.route) {
+            Text(text = "Hello Main")
+        }
+        composable(MainNavigationItem.Category.route) {
+            Text(text = "Hello Category")
+        }
+        composable(MainNavigationItem.MyPage.route) {
+            Text(text = "Hello MyPage")
+        }
+    }
 }
